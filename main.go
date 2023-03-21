e08862138d8416ef8f70bdda47d5086c8445d266
rootiens <rootiens@gmail.com>
2023-03-21 23:38:09 +0330

    some comments

--- main.go
+++ main.go
@@ -1,26 +1,35 @@
 package main
 
 import (
-	"fmt"
-
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/rootiens/telegram-downloader-uploader/utils"
 )
 
 func main() {
+    // Creates temp download directory
 	utils.CreateDownloadDir()
 
+    // Database Connection
 	utils.ConnectDB()
 
+    
 	bot, err := tgbotapi.NewBotAPI(utils.LoadEnv().Token)
 	utils.CheckErr(err)
 
+    // Request and Response logs
 	bot.Debug = true
 
+    // Create a new UpdateConfig struct with an offset of 0. Offsets are used
+    // to make sure Telegram knows we've handled previous values and we don't
+    // need them repeated.
 	updateConfig := tgbotapi.NewUpdate(0)
 
+    // Tell Telegram we should wait up to 30 seconds on each request for an
+    // update. This way we can get information just as quickly as making many
+    // frequent requests without having to send nearly as many.    
 	updateConfig.Timeout = 30
 
+    // Start polling Telegram for updates.
 	updates := bot.GetUpdatesChan(updateConfig)
 
 	for update := range updates {
